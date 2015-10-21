5feafc32ddc33b7fd5ee19d4f9b13dd6d3890130
mcyang <woohoo499@gmail.com>
2015-10-21 18:30:25 +0800

    自訂客戶群、車廠下拉選單OK 加入Enum DropDownList, 但Edit時的預選值失敗

--- APISTest/Helpers/SelectionHelper.cs
+++ APISTest/Helpers/SelectionHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using APISTest.Models;
 using APISTest.Tools;
 
 namespace APISTest.Helpers
@@ -14,22 +15,25 @@ namespace APISTest.Helpers
         /// 從 Enum Type 產生下拉選單
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="helper"></param>
-        /// <param name="Name"></param>
-        /// <param name="selected"></param>
-        /// <param name="hasAllSelection"></param>
-        /// <param name="htmlAttribute"></param>
+        /// <param name="helper">第1個參數一定要填「this HtmlHelper helper」</param>
+        /// <param name="Name">下拉選單名稱</param>
+        /// <param name="selected">選取項目</param>
+        /// <param name="hasAllSelection">是否加入「不拘」選項</param>
+        /// <param name="htmlAttribute">CSS屬性</param>
         /// <returns></returns>
         public static IHtmlString DDL_FromEnum<T>(this HtmlHelper helper, string Name, string selected, bool hasAllSelection = true, object htmlAttribute = null)
         {
-            if (string.IsNullOrEmpty(selected))
-            {
-                selected = "";
-            }
+            //建立下拉選單
             List<SelectListItem> list = new List<SelectListItem>();
+
             if (hasAllSelection)
             {
                 list.Add(new SelectListItem { Text = "請選擇", Value = "" });
+            }
+
+            if (string.IsNullOrEmpty(selected))
+            {
+                selected = "";
             }
 
             foreach (Enum item in Enum.GetValues(typeof(T)))
@@ -50,21 +54,26 @@ namespace APISTest.Helpers
         }
 
         /// <summary>
-        /// 依客戶群下拉選單
+        /// 依客戶群下拉選單-從資料庫產生
         /// </summary>
-        /// <param name="helper"></param>
-        /// <param name="Type"></param>
-        /// <param name="Name"></param>
-        /// <param name="selected"></param>
-        /// <param name="htmlAttribute"></param>
+        /// <param name="helper">第1個參數一定要填「this HtmlHelper helper」</param>
+        /// <param name="Name">下拉選單名稱</param>
+        /// <param name="selected">選取項目</param>
+        /// <param name="hasAllSelection">是否加入「不拘」選項</param>
+        /// <param name="htmlAttribute">CSS屬性</param>
         /// <returns></returns>
         public static IHtmlString DDL_CustomerTeam(this HtmlHelper helper, string Name, int selected, bool hasAllSelection = false, object htmlAttribute = null)
         {
             List<SelectListItem> list = new List<SelectListItem>(); //建立下拉選單
 
-            using (APISTest.Models.JohnTestEntities db = new APISTest.Models.JohnTestEntities())
+            if (hasAllSelection)
             {
-                foreach (var item in db.CustomerTeams.Where(p => p.IsDelete == false))
+                list.Add(new SelectListItem { Text = "不拘", Value = "" });
+            }
+
+            using (JohnTestEntities ddl_db = new JohnTestEntities())
+            {
+                foreach (var item in ddl_db.CustomerTeams.Where(p => p.IsDelete == false))
                 {
                     list.Add(new SelectListItem()
                     {
@@ -74,12 +83,40 @@ namespace APISTest.Helpers
                     });
                 }
             }
+            return helper.DropDownList(Name, list, htmlAttribute);
+        }
 
-            //if (hasAllSelection)
-            //{
-            //    list.Add(new SelectListItem { Text = "不拘", Value = "" });
-            //}
-            
+        /// <summary>
+        /// 依車廠下拉選單-從資料庫產生
+        /// </summary>
+        /// <param name="helper">第1個參數一定要填「this HtmlHelper helper」</param>
+        /// <param name="Name">下拉選單名稱</param>
+        /// <param name="selected">選取項目</param>
+        /// <param name="hasAllSelection">是否加入「不拘」選項</param>
+        /// <param name="htmlAttribute">CSS屬性</param>
+        /// <returns></returns>
+        public static IHtmlString DDL_CarMaker(this HtmlHelper helper, string Name, int? selected, bool hasAllSelection = false, object htmlAttribute = null)
+        {
+            //建立下拉選單
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            if (hasAllSelection)
+            {
+                list.Add(new SelectListItem { Text = "不拘", Value = "" });
+            }
+
+            using (JohnTestEntities ddl_db = new JohnTestEntities())
+            {
+                foreach (var item in ddl_db.CarMakers.Where(p => p.IsDelete == false))
+                {
+                    list.Add(new SelectListItem()
+                    {
+                        Text = item.Name,
+                        Value = item.ID.ToString(),
+                        Selected = selected == item.ID ? true : false
+                    });
+                }
+            }
             return helper.DropDownList(Name, list, htmlAttribute);
         }
     }
--- APISTest/Models/EnumTypes.cs
+++ APISTest/Models/EnumTypes.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 
 namespace APISTest.Models
 {
@@ -29,5 +26,69 @@ namespace APISTest.Models
         /// <summary>大億</summary>
         [Description("TY")]
         TY = 4,
+    }
+
+    /// <summary>
+    /// 幣別
+    /// </summary>
+    public enum CurrencyType
+    {
+        /// <summary>NTD</summary>
+        [Description("NTD")]
+        NTD = 1,
+
+        /// <summary>RMB</summary>
+        [Description("RMB")]
+        RMB = 2,
+    }
+
+    /// <summary>
+    /// 量產地
+    /// </summary>
+    public enum SiteType
+    {
+        /// <summary>高雄LAK</summary>
+        [Description("LAK")]
+        LAK = 1,
+
+        /// <summary>廣州LGA</summary>
+        [Description("LGA")]
+        LGA = 2,
+
+        /// <summary>墨西哥LAMX</summary>
+        [Description("LAMX")]
+        LAMX = 3,
+    }
+
+    /// <summary>
+    /// 報價類型
+    /// </summary>
+    public enum QuotationType
+    {
+        /// <summary>正式RFQ</summary>
+        [Description("正式RFQ")]
+        RFQ = 1,
+
+        /// <summary>參考報價</summary>
+        [Description("參考報價")]
+        Price_Indication = 2,
+    }
+
+    /// <summary>
+    /// 把握度
+    /// </summary>
+    public enum CertaintyType
+    {
+        /// <summary>H</summary>
+        [Description("H")]
+        H = 1,
+
+        /// <summary>M</summary>
+        [Description("M")]
+        M = 2,
+
+        /// <summary>L</summary>
+        [Description("L")]
+        L = 3,
     }
 }
