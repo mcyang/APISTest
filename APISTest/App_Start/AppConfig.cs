c47cc084bf7d088b9d5fd59e3f92bd899d836680
woohoo499 <woohoo499@gmail.com>
2015-11-12 11:10:59 +0800

    RDR表單報表重新排版

---
+++ APISTest/App_Start/AppConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APISTest.App_Start
+{
+    /// <summary>
+    /// 系統相關設定初始化
+    /// </summary>
+    public class AppConfig
+    {
+        public static ILoginUser LoginUser { get; private set; }
+
+        public static void Init()
+        {
+            LoginUser = new DefLoginUser();
+        }
+    }
+}
--- APISTest/ViewModels/RDRViewModel.cs
+++ APISTest/ViewModels/RDRViewModel.cs
@@ -1,14 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using APISTest.Models;
 
 namespace APISTest.ViewModels
 {
     public class RDRViewModel
     {
-        public RDR  rdr {get;set;}
-        public string CustomerTeamCode { get; set; }
+        //#region RDRMain相關欄位
+        //public RDRMain rdrMain { get; set; }
+
+        //public string CarMakerName { get; set; }
+
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //public DateTime RFQDate
+        //{
+        //    get { return rdrMain.RFQDate; }
+        //}
+
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //public DateTime AcquisitionDate
+        //{
+        //    get { return rdrMain.AcquisitionDate; }
+        //}
+
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
+        //public DateTime SOPDate
+        //{
+        //    get { return rdrMain.SOPDate; }
+        //}
+
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
+        //public DateTime EOLDate
+        //{
+        //    get { return rdrMain.EOLDate; }
+        //}
+
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //public DateTime CreateDate
+        //{
+        //    get { return rdrMain.CreateTime; }
+        //}
+
+        //public string EstimateRevenueText
+        //{
+        //    get
+        //    {
+        //        return APISTest.Tools.EnumMapTool.GetDescription((EnumEstimateRevenue)rdrMain.EstimateRevenue);
+        //    }
+        //}
+        //#endregion
+        public RDRMainDetailsViewModel rdrMainDetail { get; set; }
+        //public RDRModule rdrModule { get; set; }
+        //public RDRInformation rdrInfo { get; set; }
+        public List<RDRModuleDetailsViewModel> rdrModuleList { get; set; }
+
+        public RDRInformation rdrInfoDetail { get; set; }
     }
+
+    
 }
